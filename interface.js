9bfd77771a991e6d99cc26920e4595cd2cb771fd
lanzeron <lanzeron@gmail.com>
2016-04-21 11:17:40 +0300

    first commit

--- interface.js
+++ interface.js
@@ -1,6 +1,32 @@
-var config = require('./config');
-var user = require('models/user');
-var gig = require('models/gig');
-var product = require('models/product');
+var config = require('config');
+var User = require('models/user');
+var Gig = require('models/gig');
+var userjson = require('user');
+var gigjson = require('gig');
+var productjson = require('product');
+var Product = require('models/product');
 
 mongoose.connect(config.get('db_path'));
+
+exports.insert = function(){
+var user = new  User(userjson);
+user.save(function(err){
+  if (err){
+    console.log(err);
+  }
+});
+
+var gig = new Gig(gigjson);
+gig.save(function(err){
+  if (err){
+    console.log(err);
+  }
+});
+
+var product = new Product(productjson);
+product.save(function(err){
+  if (err){
+    console.log(err);
+  }
+});
+};
